562baba83206eed914b1ca813d02e8229b02494b
Guillaume Laubier <guillaume.laubier@hotmail.fr>
2023-11-24 15:12:37 +0100

    MyZip handle help option, save myzipfile and files to zip

--- MyZip/Program.cs
+++ MyZip/Program.cs
@@ -1,15 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.Immutable;
 using MyZip;
 using MyZipArchiver;
 
 if (args.Length == 0)
 {
     Helper.PrintHelpMessage();
+    return 0;
 }
 else
 {
-    Console.WriteLine("MyZip Hello World!");
+    List<string> files = new List<string>();
+    
+    foreach (string arg in args)
+    {
+        if (arg.StartsWith("-h"))
+        {
+            Helper.PrintHelpMessage();
+            return 0;
+        }
+        else if (arg.StartsWith('-'))
+        {
+            // Ignore other options
+        }
+        else
+        {
+            files.Add(arg);
+        }
+    }
 
-    var test = new Class1();
+    if (files.Count > 0)
+    {
+        var myZipFile = files[0];
+        files.RemoveAt(0);
+        return MyZipArchiver.MyZipArchiver.Zip(myZipFile, files.ToArray());
+    }
+    
+    Console.WriteLine("myzip error: Invalid command arguments. Use -h option for help.");
+    return -1;
 }
---
+++ MyZipArchiver/MyZipArchiver.cs
@@ -0,0 +1,11 @@
+using System.IO.Compression;
+
+namespace MyZipArchiver;
+
+public static class MyZipArchiver
+{
+    public static int Zip(string myZipFileName, string[] files)
+    {
+        return 0;
+    }
+}
